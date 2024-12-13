f780e192412f03b066bbeacc17de4bb843089c6b
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2024-12-13 13:09:26 +0100

    feat: Create form to edit posts

---
+++ src/components/EditPostForm.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+const postDataJson = sessionStorage.getItem("allblogPostToEdit");
+const postData = postDataJson && JSON.parse(postDataJson);
+
+export default function EditPostForm() {
+  const [title, setTitle] = useState(postData.title);
+  const [body, setBody] = useState(postData.body);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      await fetch(`http://localhost:3000/posts/${postData.id}`, {
+        method: "PUT",
+        body: JSON.stringify({ title, body }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      });
+      window.location.href = "/";
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Edit this post</h2>
+      <div>
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="body">Body</label>
+        <textarea
+          name="body"
+          id="body"
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+      </div>
+      <button type="submit">Update Post</button>
+    </form>
+  );
+}
