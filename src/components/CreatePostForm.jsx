52d1f8ac73025204408c089bd61cc3ef323ca3cc
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-01-07 11:53:52 +0100

    feat: Store jwt object received from the website domain

--- src/components/CreatePostForm.jsx
+++ src/components/CreatePostForm.jsx
@@ -1,15 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const userTokenJson = sessionStorage.getItem("apiPoweredBlogToken");
-const userToken = userTokenJson && JSON.parse(userTokenJson);
+// const userTokenJson = sessionStorage.getItem("apiPoweredBlogToken");
+// const userToken = userTokenJson && JSON.parse(userTokenJson);
 
 export default function CreatePostForm() {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
+  const [myJwt, setMyJwt] = useState("");
 
   async function handleSubmit(e) {
     e.preventDefault();
     try {
+      const userTokenJson = sessionStorage.getItem("apiPoweredBlogToken");
+      const userToken = userTokenJson && JSON.parse(userTokenJson);
       await fetch("http://localhost:3000/posts", {
         method: "POST",
         body: JSON.stringify({ title, body }),
@@ -26,28 +29,53 @@ export default function CreatePostForm() {
     }
   }
 
+  useEffect(() => {
+    const expectedOrigin = "http://localhost:4321";
+    function handleMessageEvent(e) {
+      console.log("=== handleMessageEvent ===");
+      console.log(e);
+      if (e.origin === expectedOrigin) {
+        console.log("=== useEffect === ");
+        console.log(e.data);
+
+        const jwtObj = JSON.parse(e.data);
+        if (jwtObj !== null) {
+          localStorage.setItem("apiPoweredBlogToken", JSON.stringify(jwtObj));
+          setMyJwt(JSON.stringify(jwtObj));
+        }
+      }
+    }
+    window.addEventListener("message", handleMessageEvent);
+    return () => window.removeEventListener("message", handleMessageEvent);
+  });
+
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="title">Title</label>
-        <input
-          type="text"
-          name="title"
-          id="title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="body">Body</label>
-        <textarea
-          name="body"
-          id="body"
-          value={body}
-          onChange={(e) => setBody(e.target.value)}
-        ></textarea>
-      </div>
-      <button type="submit">Add Post</button>
-    </form>
+    <>
+      {console.log(myJwt)}
+
+      <p>Joy and {myJwt}</p>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="body">Body</label>
+          <textarea
+            name="body"
+            id="body"
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          ></textarea>
+        </div>
+        <button type="submit">Add Post</button>
+      </form>
+    </>
   );
 }
