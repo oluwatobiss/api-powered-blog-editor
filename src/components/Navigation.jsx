b5046295e5d5eba4d109795c9bae5c953ed870bc
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-01-21 22:33:41 +0100

    refactor: Let only admins access the manage-staff route

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,20 +1,25 @@
-const userDataJson = localStorage.getItem("apiPoweredBlogUserData");
-const userData = userDataJson && JSON.parse(userDataJson);
-const userStatus = userData.status;
+import { useEffect, useState } from "react";
 
 export default function Navigation() {
+  const [userStatus, setUserStatus] = useState("");
+
+  useEffect(() => {
+    const userDataJson = localStorage.getItem("apiPoweredBlogUserData");
+    const userData = userDataJson && JSON.parse(userDataJson);
+    console.log("=== Navigation useEffect ===");
+    console.log(userData);
+
+    userData?.status && setUserStatus(userData.status);
+  }, []);
+
   console.log("=== Navigation ===");
-  console.log(userData);
+  console.log(userStatus);
 
   return (
     <div className="nav-links">
       <a href="/">Home</a>
       <a href="/create-post/">Create post</a>
-      {userStatus === "ADMIN" ? (
-        <a href="/manage-members/">Manage members</a>
-      ) : (
-        ""
-      )}
+      {userStatus === "ADMIN" ? <a href="/manage-staff/">Manage staff</a> : ""}
     </div>
   );
 }
