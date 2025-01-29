d503bf894b33f49a425bdf2aad9877ea71f82e45
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-01-29 12:53:46 +0100

    feat: Implement the logout logic

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,25 +1,31 @@
 import { useEffect, useState } from "react";
 
 export default function Navigation() {
+  const [userToken, setUserToken] = useState("");
   const [userStatus, setUserStatus] = useState("");
 
   function logoutUser() {
-    sessionStorage.removeItem("apiPoweredBlogToken");
+    localStorage.removeItem("apiPoweredBlogToken");
+    localStorage.removeItem("apiPoweredBlogUserData");
     window.location.href = "/";
   }
 
   useEffect(() => {
+    let userToken = localStorage.getItem("apiPoweredBlogToken");
     let userDataJson = localStorage.getItem("apiPoweredBlogUserData");
 
     console.log("=== Editor's Navigation useEffect ===");
+    console.log(userToken);
     console.log(userDataJson);
 
+    userToken === "undefined" && (userToken = undefined);
     userDataJson === "undefined" && (userDataJson = undefined);
 
     const userData = userDataJson && JSON.parse(userDataJson);
     console.log("=== Navigation useEffect ===");
     console.log(userData);
 
+    userToken && setUserToken(userToken);
     userData?.status && setUserStatus(userData.status);
   }, []);
 
@@ -29,9 +35,13 @@ export default function Navigation() {
   return (
     <div className="nav-links">
       <a href="/">Home</a>
-      {!userStatus && <a href="/sign-up">Sign up</a>}
-      {userStatus === "ADMIN" ? <a href="/manage-staff/">Manage staff</a> : ""}
-      {userStatus && (
+      {!userToken && <a href="/sign-up">Sign up</a>}
+      {userToken && userStatus === "ADMIN" ? (
+        <a href="/manage-staff/">Manage staff</a>
+      ) : (
+        ""
+      )}
+      {userToken && (
         <>
           <a href="/create-post/">Create post</a>
           <span onClick={logoutUser}>Log out</span>
